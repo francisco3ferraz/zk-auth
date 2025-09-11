af079e3f392b9dd9648382250b9e10a81c385e9f
Francisco Ferraz <francisco3ferraz@gmail.com>
2025-09-11 11:48:45 +0100

    Error response handling  so i dont have to do this later

---
+++ internal/errors/errors.go
@@ -0,0 +1,57 @@
+package errors
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type ErrorCode string
+
+const (
+	ErrCodeInternal   ErrorCode = "INTERNAL_ERROR"
+	ErrCodeNotFound   ErrorCode = "NOT_FOUND"
+	ErrCodeBadRequest ErrorCode = "BAD_REQUEST"
+)
+
+type AppError struct {
+	Code       ErrorCode `json:"code"`
+	Message    string    `json:"message"`
+	Details    string    `json:"details,omitempty"`
+	StatusCode int       `json:"-"`
+}
+
+func (e *AppError) Error() string {
+	return fmt.Sprintf("%s: %s", e.Code, e.Message)
+}
+
+func (e *AppError) WriteResponse(w http.ResponseWriter) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(e.StatusCode)
+	json.NewEncoder(w).Encode(e)
+}
+
+func NewInternalError(details string) *AppError {
+	return &AppError{
+		Code:       ErrCodeInternal,
+		Message:    "An internal error occurred",
+		Details:    details,
+		StatusCode: http.StatusInternalServerError,
+	}
+}
+
+func NewNotFoundError(resource string) *AppError {
+	return &AppError{
+		Code:       ErrCodeNotFound,
+		Message:    fmt.Sprintf("%s not found", resource),
+		StatusCode: http.StatusNotFound,
+	}
+}
+
+func NewBadRequestError(message string) *AppError {
+	return &AppError{
+		Code:       ErrCodeBadRequest,
+		Message:    message,
+		StatusCode: http.StatusBadRequest,
+	}
+}
