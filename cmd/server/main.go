f359f67bfcaa54614f96fed53ea11e9ea6d97e1d
Francisco Ferraz <francisco3ferraz@gmail.com>
2025-09-11 11:33:59 +0100

    Forgot to push main

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"github.com/francisco3ferraz/zk-auth/internal/config"
+	"github.com/francisco3ferraz/zk-auth/internal/database"
+	"github.com/francisco3ferraz/zk-auth/internal/model"
+)
+
+func main() {
+	cfg, err := config.Load()
+	if err != nil {
+		panic(err)
+	}
+
+	db, err := database.New(&cfg.Database)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	userRepo := model.NewUserRepository(db.Pool())
+	sessionRepo := model.NewSessionRepository(db.Pool())
+
+	_ = userRepo
+	_ = sessionRepo
+}
